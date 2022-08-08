d5435c8547fbe82832fe8cdc3255e5fc66b9cd15
Maicon Gerardi <maicon.gerardi@gmail.com>
2022-08-08 17:57:54 -0300

    aula 1 kafka

---
+++ 4 - Topicos Complementares/4.3 - Apache Kafka/projetos/produtor/src/main/java/com/kafka/produtor/controller/ProdutorController.java
@@ -0,0 +1,20 @@
+package com.kafka.produtor.controller;
+
+import com.kafka.produtor.service.ProdutorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/kafka")
+@RequiredArgsConstructor
+public class ProdutorController {
+
+    private final ProdutorService produtorService;
+
+    @PostMapping("/enviar")
+    public void enviarMensagemParaOTopico(String mensagem){
+        produtorService.enviarMensagemKafka(mensagem);
+    }
+}
